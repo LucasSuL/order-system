9937577b7c6176981cc3e47e542dff683539972a
Lucas Su <lijie.su93@gmail.com>
2024-07-08 18:24:51 +0930

    update datastorage

--- src/app/menu/_components/Main.jsx
+++ src/app/menu/_components/Main.jsx
@@ -6,296 +6,14 @@ import { Separator } from "@/components/ui/separator";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { Minus, Plus, ShoppingBag, ShoppingCart } from "lucide-react";
+import { Minus, Plus, ShoppingBag } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { v4 as uuidv4 } from "uuid";
+import { foods, tags } from "@/config/product";
 
-const tags = [
-  "烤串",
-  "主食",
-  "饮料",
-  "凉菜",
-  "热菜",
-  "甜品",
-  "汤类",
-  "烧烤",
-  "海鲜",
-  "素菜",
-  "肉类",
-  "小吃",
-];
-
-const foods = [
-  {
-    cat: "烤串",
-    products: [
-      {
-        name: "羊肉串",
-        name_eng: "Lamb Skewers",
-        price: "20",
-        desc: "香嫩多汁的羊肉串",
-        img: "https://picsum.photos/200",
-      },
-      {
-        name: "牛肉串",
-        name_eng: "Beef Skewers",
-        price: "30",
-        desc: "美味可口的牛肉串",
-        img: "https://picsum.photos/200",
-      },
-      {
-        name: "鸡肉串",
-        name_eng: "Chicken Skewers",
-        price: "25",
-        desc: "鲜嫩多汁的鸡肉串",
-        img: "https://picsum.photos/200",
-      },
-      {
-        name: "猪肉串",
-        name_eng: "Pork Skewers",
-        price: "22",
-        desc: "香酥入味的猪肉串",
-        img: "https://picsum.photos/200",
-      },
-      {
-        name: "蔬菜串",
-        name_eng: "Vegetable Skewers",
-        price: "15",
-        desc: "健康美味的蔬菜串",
-        img: "https://picsum.photos/200",
-      },
-    ],
-  },
-  {
-    cat: "主食",
-    products: [
-      {
-        name: "烤馒头",
-        name_eng: "Baked Mantou",
-        price: "10",
-        desc: "香脆的烤馒头",
-        img: "https://picsum.photos/200",
-      },
-      {
-        name: "烤面包",
-        name_eng: "Toasted Bread",
-        price: "15",
-        desc: "松软的烤面包",
-        img: "https://picsum.photos/200",
-      },
-    ],
-  },
-  {
-    cat: "饮料",
-    products: [
-      {
-        name: "可乐",
-        name_eng: "Cola",
-        price: "5",
-        desc: "清爽解渴的可乐",
-        img: "https://picsum.photos/200",
-      },
-      {
-        name: "雪碧",
-        name_eng: "Sprite",
-        price: "5",
-        desc: "冰凉可口的雪碧",
-        img: "https://picsum.photos/200",
-      },
-      {
-        name: "橙汁",
-        name_eng: "Orange Juice",
-        price: "6",
-        desc: "新鲜的橙汁",
-        img: "https://picsum.photos/200",
-      },
-      {
-        name: "啤酒",
-        name_eng: "Beer",
-        price: "8",
-        desc: "冰镇的啤酒",
-        img: "https://picsum.photos/200",
-      },
-    ],
-  },
-  {
-    cat: "凉菜",
-    products: [
-      {
-        name: "凉拌黄瓜",
-        name_eng: "Cucumber Salad",
-        price: "12",
-        desc: "清爽的凉拌黄瓜",
-        img: "https://picsum.photos/200",
-      },
-      {
-        name: "凉拌海带丝",
-        name_eng: "Seaweed Salad",
-        price: "15",
-        desc: "美味的凉拌海带丝",
-        img: "https://picsum.photos/200",
-      },
-    ],
-  },
-  {
-    cat: "热菜",
-    products: [
-      {
-        name: "宫保鸡丁",
-        name_eng: "Kung Pao Chicken",
-        price: "25",
-        desc: "经典的宫保鸡丁",
-        img: "https://picsum.photos/200",
-      },
-      {
-        name: "红烧排骨",
-        name_eng: "Braised Pork Ribs",
-        price: "35",
-        desc: "香嫩的红烧排骨",
-        img: "https://picsum.photos/200",
-      },
-    ],
-  },
-  {
-    cat: "甜品",
-    products: [
-      {
-        name: "冰淇淋",
-        name_eng: "Ice Cream",
-        price: "10",
-        desc: "香草味冰淇淋",
-        img: "https://picsum.photos/200",
-      },
-      {
-        name: "蛋挞",
-        name_eng: "Egg Tart",
-        price: "8",
-        desc: "酥脆的蛋挞",
-        img: "https://picsum.photos/200",
-      },
-    ],
-  },
-  {
-    cat: "汤类",
-    products: [
-      {
-        name: "西红柿鸡蛋汤",
-        name_eng: "Tomato Egg Soup",
-        price: "12",
-        desc: "营养的西红柿鸡蛋汤",
-        img: "https://picsum.photos/200",
-      },
-      {
-        name: "酸辣汤",
-        name_eng: "Hot and Sour Soup",
-        price: "14",
-        desc: "开胃的酸辣汤",
-        img: "https://picsum.photos/200",
-      },
-    ],
-  },
-  {
-    cat: "烧烤",
-    products: [
-      {
-        name: "烤鸡翅",
-        name_eng: "Grilled Chicken Wings",
-        price: "20",
-        desc: "香喷喷的烤鸡翅",
-        img: "https://picsum.photos/200",
-      },
-      {
-        name: "烤玉米",
-        name_eng: "Grilled Corn",
-        price: "10",
-        desc: "甜美的烤玉米",
-        img: "https://picsum.photos/200",
-      },
-    ],
-  },
-  {
-    cat: "海鲜",
-    products: [
-      {
-        name: "烤虾",
-        name_eng: "Grilled Prawns",
-        price: "30",
-        desc: "鲜美的烤虾",
-        img: "https://picsum.photos/200",
-      },
-      {
-        name: "烤扇贝",
-        name_eng: "Grilled Scallops",
-        price: "35",
-        desc: "美味的烤扇贝",
-        img: "https://picsum.photos/200",
-      },
-    ],
-  },
-  {
-    cat: "素菜",
-    products: [
-      {
-        name: "炒时蔬",
-        name_eng: "Stir-fried Vegetables",
-        price: "15",
-        desc: "新鲜的炒时蔬",
-        img: "https://picsum.photos/200",
-      },
-      {
-        name: "麻婆豆腐",
-        name_eng: "Mapo Tofu",
-        price: "18",
-        desc: "香辣的麻婆豆腐",
-        img: "https://picsum.photos/200",
-      },
-    ],
-  },
-  {
-    cat: "肉类",
-    products: [
-      {
-        name: "红烧牛肉",
-        name_eng: "Braised Beef",
-        price: "40",
-        desc: "浓郁的红烧牛肉",
-        img: "https://picsum.photos/200",
-      },
-      {
-        name: "香煎猪排",
-        name_eng: "Pan-fried Pork Chop",
-        price: "30",
-        desc: "美味的香煎猪排",
-        img: "https://picsum.photos/200",
-      },
-    ],
-  },
-  {
-    cat: "小吃",
-    products: [
-      {
-        name: "炸薯条",
-        name_eng: "French Fries",
-        price: "10",
-        desc: "酥脆的炸薯条",
-        img: "https://picsum.photos/200",
-      },
-      {
-        name: "鸡米花",
-        name_eng: "Popcorn Chicken",
-        price: "12",
-        desc: "美味的鸡米花",
-        img: "https://picsum.photos/200",
-      },
-    ],
-  },
-];
-
-const Main = () =>
-{
+const Main = () => {
   const [cart, setCart] = useState([]);
   const [sum, setSum] = useState(0);
-  const [serialCart, setSerialCart] = useState("");
   const [sessionId, setSessionId] = useState("");
 
   useEffect(() => {
@@ -318,8 +36,6 @@ const Main = () =>
 
   useEffect(() => {
     calcSum();
-    // Serialize cart object to a string
-    // setSerialCart(JSON.stringify(cart));
   }, [cart]);
 
   const handleClick = (product) => {
@@ -425,13 +141,20 @@ const Main = () =>
                           </Button>
                         </div>
                       ) : (
-                        <Button
-                          size="xs"
-                          className="px-2"
-                          onClick={() => handleClick(product)}
+                        <Link
+                          href={{
+                            pathname: `/menu/preview/${sessionId}`,
+                            query: { name: product.name, sum: sum },
+                          }}
                         >
-                          加入Add
-                        </Button>
+                          <Button
+                            size="xs"
+                            className="px-2"
+                            // onClick={() => handleClick(product)}
+                          >
+                            加入Add
+                          </Button>
+                        </Link>
                       )}
                     </div>
                   </div>
--- src/app/menu/page.jsx
+++ src/app/menu/page.jsx
@@ -1,38 +1,16 @@
-"use client";
+// src/app/menu/page.js
 
-import { useState } from "react";
-import { socket } from "@/utils/socket";
-import Header from "./_components/Header";
 import Main from "./_components/Main";
+import MenuLayout from "./menuLayout";
 
-const menu = [
-  { id: 1, name: "Dish 1", price: 10 },
-  { id: 2, name: "Dish 2", price: 20 },
-  // 添加更多菜品
-];
-
-const Menu = () => {
-  const [order, setOrder] = useState([]);
-  const [tableNumber, setTableNumber] = useState(0);
-
-  const handleOrder = (item) => {
-    setOrder((prevOrder) => {
-      // 确保不会重复添加同一个菜品
-      // if (prevOrder.some((orderItem) => orderItem.id === item.id)) {
-      //   return prevOrder.filter((orderItem) => orderItem.id !== item.id);
-      // } else {
-        return [...prevOrder, item];
-      // }
-    });
-  };
-
-
+const MenuPage = () => {
   return (
-    <div className="bg-white">
-      <Header />
-      <Main />
-    </div>
+    <MenuLayout>
+      <div>
+        <Main />
+      </div>
+    </MenuLayout>
   );
 };
 
-export default Menu;
+export default MenuPage;
